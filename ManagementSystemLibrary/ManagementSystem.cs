b0650e95f74b0868904496d6d49a117612406760
Joao Paulo Carvalho <acarvalho.jp@gmail.com>
2024-11-15 00:47:38 +0000

    addind booking logic to DLL

--- ManagementSystemLibrary/ManagementSystem.cs
+++ ManagementSystemLibrary/ManagementSystem.cs
@@ -8,10 +8,11 @@ namespace ManagementSystemLibrary
     {
         private List<Client> clients = new List<Client>();
         private List<Stay> stays = new List<Stay>();
+        private List<Booking> bookings = new List<Booking>();
 
         public int AddClient(Client client)
         {
-            if (client == null)
+            if (client == null || clients.Exists(c => c.id == client.Id))
             {
                 return 0;
             }
@@ -40,7 +41,7 @@ namespace ManagementSystemLibrary
 
         public List<Client> GetClients()
         {
-            return clients;
+            return new List<Client>(clients); // return a copy of the list to avoid modifying the original list
         }
 
         public Client GetClientById(int id)
@@ -55,7 +56,7 @@ namespace ManagementSystemLibrary
 
         public int AddStay(Stay stay)
         {
-            if (stay == null)
+            if (stay == null || stays.Exists(s => s.Id == stay.Id))
             {
                 return 0;
             }
@@ -84,7 +85,7 @@ namespace ManagementSystemLibrary
 
         public List<Stay> GetStays()
         {
-            return stays;
+            return new List<Stay>(stays);
         }
 
         public Stay GetStayById(int id)
@@ -95,6 +96,50 @@ namespace ManagementSystemLibrary
             }
 
             return stays.Find(stay => stay.Id == id);
+        }
+
+        public int AddBooking(Booking booking)
+        {
+            if (booking == null || bookings.Exists(b => b.Id == booking.Id))
+            {
+                return 0;
+            }
+
+            bookings.Add(booking);
+            return 1;
+        }
+
+        public int RemoveBooking(int id)
+        {
+            if (id <= 0)
+            {
+                return 0;
+            }
+
+            var bookingToRemove = bookings.Find(booking => booking.Id == id);
+
+            if (bookingToRemove == null)
+            {
+                return 0;
+            }
+
+            bookings.Remove(bookingToRemove);
+            return 1;
+        }
+
+        public List<Booking> GetBookings()
+        {
+            return new List<Booking>(bookings);
+        }
+
+        public Booking GetBookingById(int id)
+        {
+            if (id <= 0)
+            {
+                return null;
+            }
+
+            return bookings.Find(booking => booking.Id == id);
         }
     }
 }
